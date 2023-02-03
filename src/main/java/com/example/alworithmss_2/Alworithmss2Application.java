d476325bd969043c69af3c2dee6842d3d31da56d
Vereshchagin_Mikhail <farasers@mail.ru>
2023-02-03 18:14:01 +0300

    Алгоритмы_2

---
+++ src/main/java/com/example/alworithmss_2/Alworithmss2Application.java
@@ -0,0 +1,13 @@
+package com.example.alworithmss_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alworithmss2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Alworithmss2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alworithmss_2/Alworithmss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alworithmss_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alworithmss2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
